1fbd99466358825cc616a4a487aac1611a43e9ba
Jonatank28 <jonatankalmeidakk28@gmail.com>
2023-05-11 11:53:21 -0300

    ajsute env

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-console.log("env", process.env.NEXT_PUBLIC_API_URL);
 
 export const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL ? process.env.NEXT_PUBLIC_API_URL : 'https://demo.gedagro.com.br/api/'
+    baseURL: 'https://demo.gedagro.com.br/api/'
 });
