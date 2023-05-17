c4fe9a5debdde38421c34eaf0285d26c250b7dec
Roberto Delavi <roberto.delavy@gmail.com>
2023-05-17 16:32:16 -0300

    Cnfg form fornecedor

--- src/configs/api.js
+++ src/configs/api.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const api = axios.create({
     baseURL: process.env.NODE_ENV === 'development'
         ? 'http://localhost:3333/api/' // URL do backend local
-        : 'http://localhost:3333/api/' //'https://demo.gedagro.com.br/api/' // URL do backend de produção
+        : 'https://demo.gedagro.com.br/api/' // URL do backend de produção
 });
 
