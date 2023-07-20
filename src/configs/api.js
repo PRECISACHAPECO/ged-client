cd0a8123a29316a5e119f2a69afb16a8e7737565
Roberto Delavi <roberto.delavy@gmail.com>
2023-07-20 18:12:24 -0300

    URL_PRODUCTION pra cada branch

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
+import URL_PRODUCTION from './productionUrl'; //? demo ou app
 
-
-const URL_PRODUCTION = process.env.REACT_APP_URL_PRODUCTION;
 console.log("🚀 ~ URL_PRODUCTION:", URL_PRODUCTION)
 
 export const api = axios.create({
     baseURL: process.env.NODE_ENV === 'development'
         ? 'http://localhost:3333/api/'
-        : URL_PRODUCTION // 'https://demo.gedagro.com.br/api/'
+        : URL_PRODUCTION
 });
-
