a9b11c7d24d6b8316980da97c0cca91b1e17b6a3
Roberto Delavi <roberto.delavy@gmail.com>
2023-07-20 17:44:43 -0300

    stash

--- src/configs/productionUrl.js
+++ src/configs/productionUrl.js
@@ -2,4 +2,4 @@ const URL_PRODUCTION = 'https://demo.gedagro.com.br/api/';
 export default URL_PRODUCTION;
 
 //? Comando pro git ignorar o arquivo entre as branches locais (após add e commit):
-// git stash push -- "src/configs/urlProduction.js"
+// git stash
