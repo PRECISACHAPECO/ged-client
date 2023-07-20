5b0b70131d0af02580cc996fe41eb3250002e1db
Roberto Delavi <roberto.delavy@gmail.com>
2023-07-20 17:41:11 -0300

    ...

--- src/configs/urlProduction.js
+++ src/configs/urlProduction.js
@@ -1,3 +1,6 @@
 //* URL de produção (ignorado pelo git)
 const URL_PRODUCTION = 'https://app.gedagro.com.br/api/';
 export default URL_PRODUCTION;
+
+//? Comando pro git ignorar o arquivo entre as branches locais (após add):
+// git stash
