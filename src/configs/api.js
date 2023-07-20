b65d475134e2231672328ee69c5fd4743347bbac
Roberto Delavi <roberto.delavy@gmail.com>
2023-07-20 16:02:04 -0300

    URL production pegando do arquivo .env

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+console.log('api -> env: ', process.env.REACT_APP_BASE_URL_PRODUCTION)
+
 export const api = axios.create({
     baseURL: process.env.NODE_ENV === 'development'
         ? 'http://localhost:3333/api/'
