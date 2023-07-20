1b52a189f4467bfd41bf857b883ce9843dedf32e
Roberto Delavi <roberto.delavy@gmail.com>
2023-07-20 14:25:59 -0300

    Atualizaçao rota backend com app gedagro

--- src/configs/api.js
+++ src/configs/api.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const api = axios.create({
     baseURL: process.env.NODE_ENV === 'development'
         ? 'http://localhost:3333/api/' // URL do backend local
-        : 'https://demo.gedagro.com.br/api/' // URL do backend de produção
+        : 'https://app.gedagro.com.br/api/' // URL do backend de produção
 });
 
