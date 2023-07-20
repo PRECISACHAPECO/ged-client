eafeb727863b8832d86df8fb36c9ca24d02fc68d
Roberto Delavi <roberto.delavy@gmail.com>
2023-07-20 17:20:04 -0300

    ignorando arquivo urProduction

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
-import URL_PRODUCTION from './urlProduction'; //? demo ou app
+
+
+const URL_PRODUCTION = process.env.REACT_APP_URL_PRODUCTION;
 console.log("🚀 ~ URL_PRODUCTION:", URL_PRODUCTION)
 
 export const api = axios.create({
     baseURL: process.env.NODE_ENV === 'development'
         ? 'http://localhost:3333/api/'
-        : URL_PRODUCTION
+        : URL_PRODUCTION // 'https://demo.gedagro.com.br/api/'
 });
 
---
+++ src/configs/urlProduction.js
@@ -0,0 +1,3 @@
+//* URL de produção (ignorado pelo git)
+const URL_PRODUCTION = 'https://app.gedagro.com.br/api/';
+export default URL_PRODUCTION;
