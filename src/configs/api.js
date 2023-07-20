515e0f6bd3f35951e50e8d13d6679635fcb45f37
Roberto Delavi <roberto.delavy@gmail.com>
2023-07-20 16:28:01 -0300

    URL production

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import URL_PRODUCTION from './urlProduction'; //? demo ou app
+console.log("🚀 ~ URL_PRODUCTION:", URL_PRODUCTION)
 
 export const api = axios.create({
     baseURL: process.env.NODE_ENV === 'development'
