d3c319b72bedc60899820f5cd9d34e8bd4d33d56
Roberto Delavi <roberto.delavy@gmail.com>
2023-07-21 08:40:07 -0300

    Atualiação arquivo productionUrl

--- src/configs/productionUrl.js
+++ src/configs/productionUrl.js
@@ -2,4 +2,4 @@ const URL_PRODUCTION = 'https://app.gedagro.com.br/api/';
 export default URL_PRODUCTION;
 
 //? Comando pro git ignorar o arquivo entre as branches locais (após add e commit):
-// git stash
+// Criar arquivo .gitattributes na raiz do projeto e adicionar: src/configs/productionUrl.js merge=ours
